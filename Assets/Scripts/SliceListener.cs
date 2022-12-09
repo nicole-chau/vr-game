d3e32f5b08a4c1a7c622299c9b1f3d87c91f7092
nicole-chau <nicolechau524@gmail.com>
2022-12-09 15:38:53 -0500

    slicing

---
+++ Assets/Scripts/SliceListener.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliceListener : MonoBehaviour
+{
+    bool isTouched;
+    public GameObject foodHalf;
+    public GameObject hand;
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Food")) {
+            isTouched = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider collider) {
+        if (isTouched && collider.CompareTag("Food")) {
+            GameObject obj = collider.gameObject;
+
+            Vector3 pos = obj.transform.position;
+            Quaternion rot = hand.transform.rotation;
+
+            obj.SetActive(false);
+
+            Instantiate(foodHalf, pos+ new Vector3(0,0,-.2f), rot);
+            Instantiate(foodHalf, pos + new Vector3(0,0,.2f), rot);
+        }
+    }
+}
